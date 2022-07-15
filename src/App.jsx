581077e80e24aa782c318438eb3e09bad020acac
adeutou <a.deutou@gmail.com>
2022-07-15 15:10:47 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import React from "react";
+
 import { Cards, Chart, CountryPicker} from './components';
 import styles from './App.module.css';
+import { fetchData } from "./api";
+
+import coronaImage from './images/image.png'
 
 
-const App = () => {
-  return (
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+  async componentDidMount(){
+    const fetchedData = await fetchData();
+    
+    this.setState({ data: fetchedData});
+  }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({ data: fetchedData, country: country });
+
+  }
+
+  render(){
+    const { data, country } = this.state;
+
+    return (
       <div className={styles.container}>
-        <Cards />
-        <CountryPicker />
-        <Chart />
+        <img className={styles.image} src={coronaImage} alt="COVID-19" />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Cards data = {data} />
+        <Chart data={data} country={country} />
       </div>
-    )
-};
+    );
+  }
+  
+}
 
 export default App;
